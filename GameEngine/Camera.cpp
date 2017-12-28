0799e4bcec888038fb79d6fcfe13f67cf67d6142
JustBrenkman <JustBrenkman@users.noreply.github.com>
2017-12-28 16:23:07 -0700

    Added a camera class, and also included the glm library to help out with vector and matrix maths

---
+++ GameEngine/Camera.cpp
@@ -0,0 +1,126 @@
+#include "Camera.h"
+
+// Constructor with vectors
+Camera::Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+{
+	Position = position;
+	WorldUp = up;
+	Yaw = yaw;
+	Pitch = pitch;
+	updateCameraVectors();
+}
+// Constructor with scalar values
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+{
+	Position = glm::vec3(posX, posY, posZ);
+	WorldUp = glm::vec3(upX, upY, upZ);
+	Yaw = yaw;
+	Pitch = pitch;
+	updateCameraVectors();
+}
+
+// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+// FIXME TODO if the look at feature is enable return the matrix that looks at the object
+glm::mat4 Camera::GetViewMatrix()
+{
+	return glm::lookAt(Position, Position + Front, Up);
+}
+
+// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime)
+{
+	float velocity = MovementSpeed * deltaTime;
+	if (direction == FORWARD)
+		Position += Front * velocity;
+	if (direction == BACKWARD)
+		Position -= Front * velocity;
+	if (direction == LEFT)
+		Position -= Right * velocity;
+	if (direction == RIGHT)
+		Position += Right * velocity;
+}
+
+// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+void Camera::ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true)
+{
+	xoffset *= MouseSensitivity;
+	yoffset *= MouseSensitivity;
+
+	Yaw += xoffset;
+	Pitch += yoffset;
+
+	// Make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (constrainPitch)
+	{
+		if (Pitch > 89.0f)
+			Pitch = 89.0f;
+		if (Pitch < -89.0f)
+			Pitch = -89.0f;
+	}
+
+	// Update Front, Right and Up Vectors using the updated Eular angles
+	updateCameraVectors();
+}
+
+// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+void Camera::ProcessMouseScroll(float yoffset)
+{
+	if (Zoom >= 1.0f && Zoom <= 45.0f)
+		Zoom -= yoffset;
+	if (Zoom <= 1.0f)
+		Zoom = 1.0f;
+	if (Zoom >= 45.0f)
+		Zoom = 45.0f;
+}
+
+// Calculates the front vector from the Camera's (updated) Eular Angles
+// FIXME TODO calculate the look at target vector
+void Camera::updateCameraVectors()
+{
+	if (mustLookAt == false || lookAtTarget == nullptr) {
+		mustLookAt == false;
+		// Calculate the new Front vector
+		glm::vec3 front;
+		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		front.y = sin(glm::radians(Pitch));
+		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		Front = glm::normalize(front);
+		// Also re-calculate the Right and Up vector
+		Right = glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+		Up = glm::normalize(glm::cross(Right, Front));
+	}
+	else {
+
+	}
+}
+
+// Move the camera in a specific direction
+// FIXME TODO modify to look at a target point, the lookAtTarget's position
+void Camera::Move(Camera_Movement direction, float delta) {
+	float velocity = MovementSpeed * delta;
+	if (direction == FORWARD)
+		Position += Front * velocity;
+	if (direction == BACKWARD)
+		Position -= Front * velocity;
+	if (direction == LEFT)
+		Position -= Right * velocity;
+	if (direction == RIGHT)
+		Position += Right * velocity;
+}
+
+// Sets the Cameras look at target, must use function EnableLookAt() to use
+void Camera::SetCameraLookAtTarget(Object* target) {
+	lookAtTarget = target;
+};
+
+// Look At target enablers and diablers. Tells the camera wether or not to look at a target or not
+void Camera::EnableLookAt() {
+	if (lookAtTarget != nullptr) {
+		mustLookAt = true;
+	}
+};
+
+// Diables the look at function
+void Camera::DisableLookAt() {
+	mustLookAt = false;
+};
---
+++ GameEngine/Camera.h
@@ -0,0 +1,110 @@
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+//#include <glad/glad.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <vector>
+#include "Object.h"
+
+using namespace glm;
+
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVTY = 0.1f;
+const float ZOOM = 45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+
+	// Constructor with vectors
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+
+	// Constructor with scalar values
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+	// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+	glm::mat4 GetViewMatrix();
+
+	// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+
+	// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+	void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true);
+
+	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+	void ProcessMouseScroll(float yoffset);
+
+	// Move the camera in a specific direction
+	void Move(Camera_Movement dir, float delta);
+
+	// Sets the Cameras look at target, must use function EnableLookAt() to use
+	void SetCameraLookAtTarget(Object* target);
+
+	// Look At target enablers and diablers. Tells the camera wether or not to look at a target or not
+	void EnableLookAt();
+	void DisableLookAt();
+
+	// Getters and Setters
+	vec3 GetPosition() { return Position; };
+	vec3 GetFront() { return Front; };
+	vec3 GetUp() { return Up; };
+	vec3 GetRight() { return Right; };
+	vec3 GetWorldUp() { return WorldUp; };
+
+	float GetYaw() { return Yaw; };
+	float GetPitch() { return Pitch; };
+	float GetMovementSpeed() { return MovementSpeed; };
+	float GetMouseSensitivity() { return MouseSensitivity; };
+	float GetZoom() { return Zoom; };
+
+	void SetPosition(vec3 position) { Position = position; };
+	//void SetFront(vec3 front) { Front = front; };
+	//void SetUp(vec3 up) { Up = up; };
+	//void SetRight(vec3 right) { Right = right; };
+	//void SetWorldUp(vec3 worldUp) { WorldUp = worldUp; };
+
+	void SetYaw(float yaw) { Yaw = yaw; };
+	void SetPitch(float pitch) {Pitch = pitch; };
+	void SetMovementSpeed(float movementSpeed) { MovementSpeed = movementSpeed; };
+	void SetMouseSensitivity(float mouseSensitivity) { MouseSensitivity = mouseSensitivity; };
+	void SetZoom(float zoom) { Zoom = zoom; };
+
+private:
+	// Calculates the front vector from the Camera's (updated) Eular Angles
+	void updateCameraVectors();
+
+	// Camera Attributes
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+	// Eular Angles
+	float Yaw;
+	float Pitch;
+	// Camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	// LookAtTarget
+	bool mustLookAt = false;
+	Object* lookAtTarget = nullptr;
+};
+#endif
+
