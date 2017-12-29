991f4ed881a14494f97adae4801e3e2536377908
JustBrenkman <JustBrenkman@users.noreply.github.com>
2017-12-29 12:30:19 -0700

    Merge

--- GameEngine/AssetManager.h
+++ GameEngine/AssetManager.h
@@ -8,8 +8,6 @@ This is the header to manage loading assets to opengl.
 
 #include <GLEW\glew.h>
 
-#include <assimp\scene.h>
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -20,9 +18,8 @@ public:
 	void LoadShader(unsigned int& ID, const char* vertexPath, const char* fragmentPath);
 	// Parameters: ID of texture, filepath to image
 	void LoadTexture(unsigned int& texture, const char* file);
-	void LoadModel(const char* pFile); // not complete, currently working on getting assimp to work
+	void LoadModel(); // not complete, currently working on getting assimp to work
 
 private:
 	void CheckCompileErrors(GLuint shader, std::string type);
-	void ProcessScene(const aiScene* scene);
 };
