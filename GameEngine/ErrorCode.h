1017e68dd8fae42996c1f0e1faee6e3cd2f11503
Jon Meilstrup <jon.d.meilstrup@gmail.com>
2018-04-10 22:19:01 -0600

    Updated with comments: -CrashHandler.h -CrashHandler.cpp -ExceptionTemplate.h -ErrorCode.h -ErrorCode.cpp
    Created:
    -ExampleException.cpp
    -ExceptionInclude.cpp

--- GameEngine/ErrorCode.h
+++ GameEngine/ErrorCode.h
@@ -5,6 +5,25 @@ using std::ostream;
 using std::stringstream;
 using std::string;
 
+/*
+ * Author: Jon Meilstrup
+ *
+ * Includes class declaration for ErrorCode class.
+ *
+ * This is used to keep identify any errors thrown in our code. It has an alphanumeric code (e.g. A101), a short summary
+ * describing the error, and some example text going more in-depth into the error and showing possible ways the error
+ * may have happened.
+ *
+ * string toString() const: Converts the ErrorCode into a string
+ *		-e.g. "A101: Example error"
+ *
+ * friend ostream& operator<<(ostream& os, const ErrorCode& e): Makes it possible to insert the ErrorCode into an ostream.
+ *		-e.g. ostream result;
+ *		      ExampleException e;
+ *		      result << e;
+ *			  return result;
+ */
+
 class ErrorCode
 {
 private:
---
+++ GameEngine/ExampleException.cpp
@@ -0,0 +1,31 @@
+#include "ExceptionTemplate.h"
+#include "ErrorCode.cpp"
+
+/*
+ * Author: Jon Meilstrup
+ *
+ * Example of an exception class
+ * 
+ * Be sure to write '#include "ExampleException.cpp"' in the "ExceptionInclude.cpp" file, replacing "ExampleException" with the name
+ * of the exception class you write.
+ *
+ * When initializing the exception, new up the ErrorCode, where the arguments are char type, int code, string summary, and
+ * string example. These are used when logging errors to show the error code itself in the type and code, the summary describing
+ * a basic one-phrase or one-sentence description of the error thrown, and the example text going more in-depth into the
+ * error and why it might exist.
+ */
+
+class ExampleException : public ExceptionTemplate
+{
+public:
+	string errorMessage;
+	ErrorCode* errorCode;
+	ExampleException()
+	{
+		errorCode = new ErrorCode('A', 101, "Example error", "This is an example error. You will never actually get an A101.");
+	};
+	~ExampleException()
+	{
+		delete errorCode;
+	};
+};
