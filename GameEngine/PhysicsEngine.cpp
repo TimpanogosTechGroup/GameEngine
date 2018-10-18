b3bfc7f72c74cf70392a4853e2825ab48256b783
JustBrenkman <JustBrenkman@gmail.com>
2018-10-18 16:37:00 -0600

    Physics engine is giving us some difficulties, keeps crashing the game everytime the setSimulation is called.

--- GameEngine/PhysicsEngine.cpp
+++ GameEngine/PhysicsEngine.cpp
@@ -15,21 +15,21 @@
 using namespace std;
 
 void PhysicsEngine::Update(double delta, ModelManager& modelManager) {
-	dynamicsWorld->stepSimulation(delta / 2000, 5);
-
-	//for (int i = 0; i < modelManager.size(); i++) {
-	//	btTransform trans;
-	//	rigidBodies.at(i+1)->getMotionState()->getWorldTransform(trans);
-	//	modelManager.GetModel(i)->SetPosition(glm::vec3(trans.getOrigin().x(), trans.getOrigin().y(), trans.getOrigin().z()));
-	//	modelManager.GetModel(i)->SetTransform(trans);
-	//}
-
-	for (auto &iter : modelManager.getPhysicalInstances()) {
-		btTransform trans;
-		rigidBodies[iter.second->getName()]->getMotionState()->getWorldTransform(trans);
-		//std::cout << rigidBodies[iter.second->getName()]->getMotionState()->getWorldTransform(trans) << endl;
-		//iter.second->setInstancePosition(glm::vec3(trans.getOrigin().x(), trans.getOrigin().y(), trans.getOrigin().z()));
-		iter.second->setInstanceTrasnform(trans);
-		//iter.second->setInstanceTrasnform(trans);
-	}
+//	dynamicsWorld->stepSimulation(delta / 2000, 5);
+//
+//	//for (int i = 0; i < modelManager.size(); i++) {
+//	//	btTransform trans;
+//	//	rigidBodies.at(i+1)->getMotionState()->getWorldTransform(trans);
+//	//	modelManager.GetModel(i)->SetPosition(glm::vec3(trans.getOrigin().x(), trans.getOrigin().y(), trans.getOrigin().z()));
+//	//	modelManager.GetModel(i)->SetTransform(trans);
+//	//}
+//
+//	for (auto &iter : modelManager.getPhysicalInstances()) {
+//		btTransform trans;
+//		rigidBodies[iter.second->getName()]->getMotionState()->getWorldTransform(trans);
+//		//std::cout << rigidBodies[iter.second->getName()]->getMotionState()->getWorldTransform(trans) << endl;
+//		//iter.second->setInstancePosition(glm::vec3(trans.getOrigin().x(), trans.getOrigin().y(), trans.getOrigin().z()));
+//		iter.second->setInstanceTrasnform(trans);
+//		//iter.second->setInstanceTrasnform(trans);
+//	}
 }
